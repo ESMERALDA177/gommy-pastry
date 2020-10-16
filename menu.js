14a2c221c2fad7baf3c939713aaee2cd07e4b9da
ESMERALDA177 <72953662+ESMERALDA177@users.noreply.github.com>
2020-10-15 22:05:46 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,13 @@
+const hamburguesa = document.querySelector('.hamburguesa');
+const menu= document.querySelector('.menu-navegacion');
+console.log(menu)
+console.log(hamburguesa)
+hamburguesa.addEventListener('click',()=>{
+    menu.classList.toggle("spread")
+
+})
+
+window.addEventListener('click',()=>{
+    menu.classList.toggle("spread")
+   
+})
